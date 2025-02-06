883ab20468fdd1533c7cdac113004803c0041d7d
SrMorim <felipe.amorim@tutamail.com>
2025-02-06 13:52:15 -0300

    Java Update

---
+++ Java/Scripts/srcA/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+
+    static void HelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    
+
+
+}
