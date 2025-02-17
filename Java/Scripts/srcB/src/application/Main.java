bf21ae7bec669431dd7d068d511effb44ce3998f
SrMorim <felipe.amorim@tutamail.com>
2025-02-17 00:02:56 -0300

    Java Update

--- Java/Scripts/srcB/src/application/Main.java
+++ Java/Scripts/srcB/src/application/Main.java
@@ -1,11 +1,13 @@
 package application;
 
 import entities.*;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         Triangle x = new Triangle();
@@ -20,5 +22,7 @@ public class Main {
         System.out.print("Declare o segundo valor do trianguloY: ");y.b = sc.nextDouble();
         System.out.print("Declare o terceiro valor do trianguloY: ");y.c = sc.nextDouble();
 
+        System.out.println("\nA área do triangulo X é: " + x.area());
+        System.out.println("A área do triangulo Y é: " + y.area());
     }
 }
