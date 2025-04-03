82d87d63a7b4bcd27c76a098b95b014248a81391
Felipe Amorim Monteiro <felipe.am@dinfor.uniceub>
2025-04-03 09:19:02 -0300

    .

--- Studies/Python/MVCPython/controller/main_controller.py
+++
@@ -1,9 +0,0 @@
-from model.usuario import Usuario
-from view.main_view import MainView
-
-class MainController:
-    def __init__(self):
-        self.view = MainView()
-    
-    def run(self):
-        self.view.show_main_screen()
--- Studies/Python/MVCPython/view/main_view.py
+++
@@ -1,3 +0,0 @@
-class MainView:
-    def show_main_screen(self):
-        print("Bem-vindo ao sistema! Escolha uma opção:")
