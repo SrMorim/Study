eb4dbfbfc1ebbaaee8f6b9ee6c24949e928a0388
SrMorim <felipe.am@sempreceub.com>
2025-06-04 21:48:33 -0300

    commit 1

---
+++ Studies/Java/commit.java
@@ -0,0 +1,11 @@
+package applications;
+
+import entities.*;
+import java.util.*;
+
+//Code
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Commit 1");
+    }
+}
