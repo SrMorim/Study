6a6166b98f0cc0a2e09b6722217d52ed0ac42c82
SrMorim <felipe.am@sempreceub.com>
2025-06-13 22:11:27 -0300

    .

---
+++ Studies/Development/Python/intro/srcA.py
@@ -0,0 +1,30 @@
+def ex1():
+    nome = 'felipe'
+    sobrenome = 'monteiro'
+    idade = 99
+    ano_nascimento = 2099
+    maioridade = 'sim'
+    altura_metros = 3.20
+
+    print("Nome:", nome)
+    print("Sobrenome:", sobrenome)
+    print("Idade:", idade)
+    print("Ano de nascimento:", ano_nascimento)
+    print("Maior de idade?:", "sim")
+    print("Altura em metros:", altura_metros)
+
+def ex2():
+    nome = input("Digite seu nome: ")
+    altura = float(input("Digite sua altura em metros:"))
+    peso = int(input("Digite seu peso em kg:"))
+
+    imc = peso/altura**2
+
+    print(f"{nome} tem {altura} de altura, pesa {peso} e seu IMC é {imc}")
+
+def ex3():
+    
+    
+
+if __name__ == "__main__":
+    ex2()
